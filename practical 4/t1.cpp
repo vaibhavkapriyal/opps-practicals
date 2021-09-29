2cb63e096fd11e95fdbcad2234116ce8c1eae4a1
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-09-29 10:22:14 +0530

    Create t1.cpp

---
+++ practical 4/t1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+namespace first
+{
+	int add(int a,int b)
+	{
+		return a+b;
+	}
+}
+namespace second
+{
+	float add(float a,float b)
+	{
+		return a+b;
+	}
+}
+//using namespace second;
+//using namespace first;
+int main()
+{
+	//cout<<first::add(12,15)<<"\n";
+	//cout<<second::add(12.6,15.9)<<"\n";
+	cout<<add(12,15)<<"\n";
+	return 0;
+}
