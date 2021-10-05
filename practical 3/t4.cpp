e2b4fcaa7687f7e519928b3f05ea3bd30dc6438e
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-10-05 22:15:06 +0530

    Create t4.cpp

---
+++ practical 3/t4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cout<<"Enter a series of character\n";
+	getline(cin,str);
+	cout<<"Display:-"<<str<<"\n";
+	return 0;
+}
+
