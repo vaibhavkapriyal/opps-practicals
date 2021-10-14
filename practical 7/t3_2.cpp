7810e7a57d0cf48816ca373b72e1cb52dddcc6ba
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-10-14 10:03:59 +0530

    Create t3_2.cpp

---
+++ practical 7/t3_2.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cout<<"Enter size of the array:-\n";
+	cin>>n;
+	int a[n][n];
+	cout<<"Enter the elements of the array\n";
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	cout<<"Reversed Array is :-\n";
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			cout<<10-a[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
