11189a1d143b3fea5995cd413c9a909aef7e4c8b
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-10-19 11:03:50 +0530

    Create t4_2.cpp

---
+++ practical 7/t4_2.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cout<<"Enter size of the array:-\n";
+	cin>>n;
+	int a[n][n],b[n][n];
+	cout<<"Enter the elements of the array\n";
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	cout<<"New matrix is:\n";
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cout<<a[i][n-1]/3<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
