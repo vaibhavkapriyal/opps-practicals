7b73b9b6efdda5c8bae1cebbce2d42ba346519d7
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-10-20 09:47:35 +0530

    Create t6.cpp

---
+++ practical 7/t6.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i,j;
+	cout<<"Enter size of the array:-\n";
+	cin>>n;
+	char a[n][n];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<i;j++)
+			a[i][j]=' ';
+		for(j=i;j<n;j++)
+			a[i][j]='*';
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<'\n';
+	}
+	return 0;
+}
