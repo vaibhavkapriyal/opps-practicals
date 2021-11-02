6a4959ae8aa8dba450b36c79ce16229c787571ae
vaibhavkapriyal <91269085+vaibhavkapriyal@users.noreply.github.com>
2021-11-02 11:02:14 +0530

    Create t6.cpp

---
+++ practical 9/t6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () 
+{
+cout<<"Vaibhav Kumar Kapriyal"<<endl;
+
+cout<<setw(15)<<200111163<<endl;
+
+cout<<setfill('*')<<setw(15)<<200111163<<endl;
+return 0;
+}
